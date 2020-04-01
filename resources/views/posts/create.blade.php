da22148978ccff44ba7f6ecb21b5cff26a6a4cf0
viktor-osvoll <viktor.osvoll@medieinstitutet.se>
2020-04-01 18:54:20 +0200

    added front-end to create-post and img-adding

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,22 +3,31 @@
 @section('content')
 <div class="wrapper create-post">
     <h1 style="text-align: center; margin-top: 5%;">Create a New Post</h1>
-    <form action="/posts" method="POST" style="text-align: center;">
+    <form action="/posts" method="POST" enctype="multipart/form-data" style="width: 80%; margin-left: 10%;">
         @csrf
-        <label for="title">Title:</label> <br />
-        <input type="text" name="title" id="title"> <br />
-        <label for="content">Content:</label><br />
-        <input type="text" name="content" id="content"><br />
-        <label for="subreddits" style="margin-top: 2%;">Subreddits:</label>
-        <select name="subreddit" id="subreddit">
-            <option value="animals">Animals</option>
-            <option value="vechiles">Vechiles</option>
-            <option value="properties">Properties</option>
-            <option value="electronics">Electronics</option>
-            <option value="garden">Garden</option>
-        </select><br />
-        <input type="submit" value="Publish" style="margin-top: 1%;">
-
-    </form>
+        <div class="form-group">
+          <label for="title">Title:</label>
+          <input type="title" class="form-control" id="title" placeholder="Title">
+        </div>
+        <div class="form-group">
+          <label for="subreddit">Subreddit:</label>
+          <select class="form-control" id="subreddit">
+            <option>Gaming</option>
+            <option>Animals</option>
+            <option>Cars</option>
+            <option>Food</option>
+            <option>Coding</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="content">Content:</label>
+          <textarea class="form-control" id="content" rows="6" placeholder="What is this post about?"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlFile1">Example file input</label>
+            <input type="file" class="form-control-file" id="exampleFormControlFile1">
+          </div>
+          <button type="submit" name="submit" class="btn btn-primary"> Post </button>
+      </form>
 </div>
 @endsection
